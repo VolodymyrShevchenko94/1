6ab0d2534f8215dde56fd905d74d87cbd0c20b79
volodymyrshevchenko <Chief_94@ukr.net>
2023-06-07 21:38:01 +0300

    ex3

--- src/Ex3/Main.java
+++ src/Ex3/Main.java
@@ -8,23 +8,35 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> myNumbers = new ArrayList<>();
-        myNumbers.add(1);
-        myNumbers.add(2);
-        myNumbers.add(3);
-        myNumbers.add(5);
-        myNumbers.add(6);
-        myNumbers.add(7);
-        myNumbers.add(8);
-        myNumbers.add(9);
         myNumbers.add(10);
+        myNumbers.add(20);
+        myNumbers.add(30);
+        myNumbers.add(40);
+        myNumbers.add(50);
+        myNumbers.add(60);
+        myNumbers.add(70);
+        myNumbers.add(80);
+        myNumbers.add(90);
 
         ListIterator listIterator = myNumbers.listIterator();
         while(listIterator.hasNext()) {
-//            Objects element = listIterator.next();
-//            listIterator.set(element +1);
+            myNumbers.set(0,11);
+            myNumbers.set(1,21);
+            myNumbers.set(2,31);
+            myNumbers.set(3,41);
+            myNumbers.set(4,51);
+            myNumbers.set(5,61);
+            myNumbers.set(6,71);
+            myNumbers.set(7,81);
+            myNumbers.set(8,91);
+
+
+            System.out.println(listIterator.next());
+
 
 
         }
+
 
 
         }
